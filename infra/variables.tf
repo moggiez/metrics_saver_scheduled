137cd0768c89423f78252be44bb117a589d942a6
GeorgiStavrev <georgi@insurify.com>
2021-08-21 10:18:36 +0300

    fix infra formatting

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,19 +1,19 @@
 variable "domain_name" {
-    type    = string
-    default = "moggies.io"
+  type    = string
+  default = "moggies.io"
 }
 
 variable "project_name" {
-    type    = string
-    default = "metrics_saver_scheduled"
+  type    = string
+  default = "metrics_saver_scheduled"
 }
 
 variable "region" {
-    type    = string
-    default = "eu-west-1"
+  type    = string
+  default = "eu-west-1"
 }
 
 variable "account" {
-    type    = string
-    default = "989665778089"
+  type    = string
+  default = "989665778089"
 }
